c9e9e9005c698e1b4738eb7ce6acea9ad7e8722b
Phil Hendren <phil.hendren@mindcandy.com>
2014-01-22 15:22:58 +0000

    Add month_by_day_by_tag method to report by custom tags

--- NHDH/daily.py
+++ NHDH/daily.py
@@ -24,6 +24,38 @@ class Daily():
         ]
         return d
 
+    def month_by_day_by_tag(self, filename, tag_key, value, fill=None):
+        """Report on custom tag key/value pair. Optional fill null values in the
+        tag_key column with a default entry e.g. "general"
+        Returns two dictionaries containing cumulative cost and detailed cost"""
+        cumulative = False
+        detailed = False
+        try:
+            file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            df = pd.read_csv(file, index_col='UsageStartDate', parse_dates=True, header=0)
+            if fill is not None:
+                df = df.fillna({tag_key:%s %(fill)})
+            df = df[np.isfinite(df['SubscriptionId'])]
+            gb = df.groupby(by='user:%s' % tag_key)
+            gb = gb.get_group(value)
+            
+            # work out product detailed breakdown for current month so far
+            db = gb.groupby(['ItemDescription']).sum().sort('UnBlendedCost', ascending=False)
+            jbb = db[['UnBlendedCost']]
+            jbb['Cumulative'] = jbb['UnBlendedCost'].cumsum()
+            detailed = {value: jbb}
+
+            # work out cumulative product cost for current month so far
+            pb = gb.groupby([lambda x: x.day]).sum()
+            jb = pb[['UnBlendedCost']]
+            jb['Change'] = jb['UnBlendedCost'].pct_change()
+            jb['Cumulative'] = jb['UnBlendedCost'].cumsum()
+            cumulative = {value: jb}
+        except:
+            pass
+        return cumulative, detailed
+
+
     def month_by_day(self,filename):
         file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         df = pd.read_csv(file, index_col='UsageStartDate', parse_dates=True, header=0)
